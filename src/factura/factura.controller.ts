0ff5b5bab5b9dd23f0d3cbcf147d575f886eca88
mauricio dawbarn <mauriciodawbarn88@gmail.com>
2019-07-28 22:25:11 -0300

    modificacion de la ruta de controlador de factura

--- src/factura/factura.controller.ts
+++ src/factura/factura.controller.ts
@@ -3,7 +3,7 @@ import { BaseController } from 'src/generico/base.controller';
 import { Factura } from './interfaces/factura.interface';
 import { FacturaService } from './factura.service';
 
-@Controller('ai/v1/facturas')
+@Controller('api/v1/facturas')
 export class FacturaController extends BaseController<Factura>{
 
     constructor(private _facturaService: FacturaService){
